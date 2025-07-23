fbf2fa87210429ad3786e1612a55260ad72e30f4
kristanrey <kristanreyflores08@gmail.com>
2025-07-24 01:32:21 +0800

    Updates

--- assets/ios.transition-legacy-CaAd4ztV.js
+++ assets/ios.transition-legacy-CaAd4ztV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BmUz0Ycl.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-CaCUU-vg.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
