eb8e242e8db4bf9f7cfce6ba65a2224f4529b920
kristanrey <kristanreyflores08@gmail.com>
2025-07-24 00:50:09 +0800

    added new file login.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
-/* Core CSS required for Ionic components to work properly */
+
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
@@ -19,19 +14,11 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
-/* Theme variables */
 import './theme/variables.css';
+
+import Login from './pages/Login';
+import Menu from './pages/Menu';
+import Registration from './pages/Registration';
 
 setupIonicReact();
 
@@ -39,12 +26,9 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/GreenPoints/home">
-          <Home />
-        </Route>
-        <Route exact path="/GreenPoints">
-          <Redirect to="/GreenPoints/home" />
-        </Route>
+        <Route exact path="/GreenPoints" component={Login} />
+        <Route path="/GreenPoints/register" component={Registration} />
+        <Route path="/GreenPoints/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
