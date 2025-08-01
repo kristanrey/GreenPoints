5ac9aec21732c3e61c448087a6345ce2b5b70d64
kristanrey <kristanreyflores08@gmail.com>
2025-08-01 21:21:25 +0800

    added update in my app.tsx/login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+// src/App.tsx
 import { Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // ✅ Required!
 
 import '@ionic/react/css/core.css';
 import '@ionic/react/css/normalize.css';
@@ -14,22 +16,25 @@ import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
 import './theme/variables.css';
+
 import Login from './pages/Login';
 import Menu from './pages/Menu';
-import Registration from './pages/Register'; // Import Registration component
+import Registration from './pages/Register';
 
 setupIonicReact();
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/GreenPoints" component={Login} />
-        <Route path="/GreenPoints/register" component={Registration} />
-        <Route path="/GreenPoints/app" component={Menu} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
+  <GoogleOAuthProvider clientId="871717818296-fbp2i05vu79pps91uhfdb50d3s2764km.apps.googleusercontent.com"> {/* ✅ REQUIRED */}
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/GreenPoints" component={Login} />
+          <Route path="/GreenPoints/register" component={Registration} />
+          <Route path="/GreenPoints/app" component={Menu} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  </GoogleOAuthProvider>
 );
 
 export default App;
