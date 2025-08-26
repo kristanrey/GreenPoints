2b211213717bdacb51143af8f7d998313fb7d24d
kristanrey <kristanreyflores08@gmail.com>
2025-08-26 19:43:03 +0800

    Updates

--- assets/md.transition-legacy-BUfBnv6L.js
+++ assets/md.transition-legacy-BUfBnv6L.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DoCGpwl7.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
+System.register(["./index-legacy-BtZqXCzf.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
