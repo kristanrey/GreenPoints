2df1cbf7b63f2745aa6b1bbf61917e01aee8c882
kristanrey <kristanreyflores08@gmail.com>
2025-09-12 19:28:33 +0800

    Updates

--- assets/swipe-back-legacy-mb-O-ddC.js
+++ assets/swipe-back-legacy-mb-O-ddC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BDe6bzPI.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-DT88xb3p.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
