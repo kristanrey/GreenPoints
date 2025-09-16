2296c46124f66090d250e4964939656f4c863806
kristanrey <kristanreyflores08@gmail.com>
2025-09-16 16:17:40 +0800

    Updates

--- assets/swipe-back-legacy-DErEwpYp.js
+++ assets/swipe-back-legacy-DErEwpYp.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-FRCnkdLS.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-0c3Cbj36.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
