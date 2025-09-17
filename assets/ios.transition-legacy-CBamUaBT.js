ea28d8025569e03d1fe8f85780d894731ccc75ef
kristanrey <kristanreyflores08@gmail.com>
2025-09-17 16:11:12 +0800

    Updates

--- assets/ios.transition-legacy-CBamUaBT.js
+++ assets/ios.transition-legacy-CBamUaBT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BxEAeYXq.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
+System.register(["./index-legacy-BTI2jW6H.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
