395d311b3a5a54aa2aecd88678665633ad899eb8
kristanrey <kristanreyflores08@gmail.com>
2025-09-25 16:22:52 +0800

    Updates

--- assets/md.transition-legacy-jcHspVfY.js
+++ assets/md.transition-legacy-jcHspVfY.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cjf8R9Gm.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
+System.register(["./index-legacy-DWXrW2U2.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
