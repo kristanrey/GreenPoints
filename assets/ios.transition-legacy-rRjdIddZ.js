cbd4bf1d5a5fbde068b202109224242e4073d120
kristanrey <kristanreyflores08@gmail.com>
2025-10-02 16:46:32 +0800

    Updates

--- assets/ios.transition-legacy-rRjdIddZ.js
+++ assets/ios.transition-legacy-rRjdIddZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BdJOR0W5.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
+System.register(["./index-legacy-Z15T0Pjl.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
