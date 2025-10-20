66f5970b8df827fd5641b0ea2bbb3fb9baf28597
kristanrey <kristanreyflores08@gmail.com>
2025-10-20 09:45:28 +0800

    added update

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -27,7 +27,7 @@
               </IonText>
 
               <img
-                src="https://scontent.fcgy4-1.fna.fbcdn.net/v/t1.15752-9/522341098_1094714478913591_4251909951679950225_n.png?_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeGWIpOxQ_W3lYt9sxtKQdADrb9lzD4spMmtv2XMPiykyU2qRncsDps76OTLE1sDLg4T6KkFROMU_Z2nGj1xCFA3&_nc_ohc=fWPFbGF1V7QQ7kNvwFqHAQa&_nc_oc=AdnMGO0ThicGoG4CsKxFN-Wfe8n99BmgdqdW9GUB2A5tlRMlu0Mw4bfwUuRTqJ0bRHAIIn11k3HGAKmXe-Wgl7YI&_nc_ad=z-m&_nc_cid=5917&_nc_zt=23&_nc_ht=scontent.fcgy4-1.fna&oh=03_Q7cD3QG3oo91ZFj338i-YG5i-Q22dYjqhwFSVBSQhKpG0GINWw&oe=68DE6737"
+                src="https://scontent.fdvo6-1.fna.fbcdn.net/v/t1.15752-9/522341098_1094714478913591_4251909951679950225_n.png?_nc_cat=105&ccb=1-7&_nc_sid=0024fc&_nc_eui2=AeGWIpOxQ_W3lYt9sxtKQdADrb9lzD4spMmtv2XMPiykyU2qRncsDps76OTLE1sDLg4T6KkFROMU_Z2nGj1xCFA3&_nc_ohc=n8rjiasNt2YQ7kNvwET0GLB&_nc_oc=AdmfWnvjhWTMrUHQDGfZhpjpUHl7p05AY-QAcesVJYR-OnX9KCPpsUMB9pn-5qZ4Fhs&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fdvo6-1.fna&oh=03_Q7cD3gG2upQFKDLXfwHThg7qckMiMjAQXAleF2FD2dMEJVYCcQ&oe=691D0677"
                 alt="Tree Icon"
                 className="tree-icon"
                 style={{ width: "250px", height: "250px" }} // 👈 test override
