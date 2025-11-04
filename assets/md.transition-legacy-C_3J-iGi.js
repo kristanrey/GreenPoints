1720c1bf815c0dfa6a238b2720c78e11ace5413d
kristanrey <kristanreyflores08@gmail.com>
2025-11-04 15:54:33 +0800

    Updates

--- assets/md.transition-legacy-C_3J-iGi.js
+++ assets/md.transition-legacy-C_3J-iGi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C6fwo15C.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
+System.register(["./index-legacy-DexPYxtz.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
