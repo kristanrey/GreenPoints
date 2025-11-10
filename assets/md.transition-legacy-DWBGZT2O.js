12c0d8c5a0d31e63459df7a2fa76d1a9707a6da2
Joanbensulan <joanbensulan2@gmail.com>
2025-11-11 01:05:08 +0800

    Updates

--- assets/md.transition-legacy-DWBGZT2O.js
+++ assets/md.transition-legacy-DWBGZT2O.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D-13Qp2I.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
+System.register(["./index-legacy-DiDluBdj.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
