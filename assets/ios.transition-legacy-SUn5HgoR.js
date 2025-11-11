9e0d544258df101081160a10523d68049fe9279d
Joanbensulan <joanbensulan2@gmail.com>
2025-11-11 16:22:53 +0800

    Updates

--- assets/ios.transition-legacy-SUn5HgoR.js
+++ assets/ios.transition-legacy-SUn5HgoR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dg41PT85.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
+System.register(["./index-legacy-aH_Sxc9_.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-CItaco2K.js
+++ assets/swipe-back-legacy-CItaco2K.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dg41PT85.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-aH_Sxc9_.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
