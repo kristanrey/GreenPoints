dc3514c894144e3587d73dfde5357db4d347b5a4
Joanbensulan <joanbensulan2@gmail.com>
2025-11-11 16:47:21 +0800

    Updates

--- assets/md.transition-legacy-CHk4rzME.js
+++ assets/md.transition-legacy-CHk4rzME.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-aH_Sxc9_.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
+System.register(["./index-legacy-BCa4pBqF.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
